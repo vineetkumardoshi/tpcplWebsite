1530e4b7c167ef87475f7b70348c0343e91100da
Vineet Kumar Doshi <vineetkumardoshi@users.noreply.github.com>
2021-09-17 08:53:57 +0530

    Added Brand Logos

--- js/product.js
+++ js/product.js
@@ -1,9 +1,18 @@
 
 var productAreaElement = document.getElementById("productArea")
 
+productAreaElement.innerHTML = `<center><h1 class="productHeading">Brands</h1></center><br><br>`
+brandList.forEach((brandName,index)=>{
+		productAreaElement.innerHTML += `<div class="product" id="P${index+1}">
+										<a href="#" onclick="showProduct(this)"><img src="img/brands/${brandName}.jpg"><br>
+										<center><div class="center" style="display:block !important;bottom:10%;width:100%;position:absolute;font-weight:normal;border: 1px solid black;">${brandName}</div></center>
+								 </a></div>`;						 	
+	});
+	productAreaElement.innerHTML += "<br><br><br><br><br><br>"
 var showProduct = (productClicked) => {
 
-		var productSelected = productClicked.children[0].textContent.trim(); //Removing unecessary tags to capture only name of product
+		productAreaElement.innerHTML = "";
+		var productSelected = productClicked.children[productClicked.children.length-1].textContent.trim() //Removing unecessary tags to capture only name of product
 		productAreaElement.innerHTML = `<center><h1 class="productHeading">${productSelected}</h1></center><br><br>`
 		products.forEach((productName, index)=>{
 			if(productName.includes(productSelected)){
